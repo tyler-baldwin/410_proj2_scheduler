31ad4a32cc24b2bb7aff03e18a388f36adb23114
tyler-baldwin <tyler.baldwin.17@cnu.edu>
2020-02-20 10:20:29 -0500

    start debug

--- scheduler/scheduler_RR.cpp
+++ scheduler/scheduler_RR.cpp
@@ -12,6 +12,7 @@
 #include "../includes/scheduler_RR.h"
 
 bool Scheduler_RR::time_to_switch_processes(int tick_count, PCB &p) {
+	return Scheduler::time_to_switch_processes(tick_count, p);
 }
 
 void Scheduler_RR::sort() {
--- scheduler/scheduler_SRTF.cpp
+++ scheduler/scheduler_SRTF.cpp
@@ -1,18 +1,20 @@
-#include "../includes/scheduler_SRTF.h"
-
-bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
-}
-
-void Scheduler_SRTF::sort() {
-}
 /*
  * SchedulerSRTF.cpp
  *
- *  Created on: Sep 10, 2019
- *      Author: keith
+ *  Created on: 2020AD
+ *      Author:TBALDY
  *
  *      implements shortest remaining time first scheduling algorithm
  *     pre-emptive
  */
 
 //TODO fill in content
+
+#include "../includes/scheduler_SRTF.h"
+
+bool Scheduler_SRTF::time_to_switch_processes(int tick_count, PCB &p) {
+	return Scheduler::time_to_switch_processes(tick_count, p);
+}
+
+void Scheduler_SRTF::sort() {
+}
