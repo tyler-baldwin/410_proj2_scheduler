f7579a211b5057a686b9ea5ee9d78d867facce89
tyler-baldwin <tyler.baldwin.17@cnu.edu>
2020-02-20 18:38:46 -0500

    round robin babyyyyy

--- scheduler/scheduler_RR.cpp
+++ scheduler/scheduler_RR.cpp
@@ -8,15 +8,21 @@
  *      pre-emptive
  */
 
-
 #include "../includes/scheduler_RR.h"
 
 bool Scheduler_RR::time_to_switch_processes(int tick_count, PCB &p) {
-
+	if (p.remaining_cpu_time <= 0) {
+		return true;
+	} else if (p.required_cpu_time != p.remaining_cpu_time
+			&& (p.required_cpu_time - p.remaining_cpu_time)
+					% time_slice == 0) {
+		return true;
+	} else {
+		return false;
+	}
 	//return Scheduler::time_to_switch_processes(tick_count, p);
 }
 //RR - preemptive - no sorting of ready_q needed.
 void Scheduler_RR::sort() {
 }
-
 
