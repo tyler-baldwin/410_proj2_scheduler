6a1458156bcbb4d906e0e2c8ed7df1dacf430701
tyler-baldwin <tyler.baldwin.17@cnu.edu>
2020-02-19 22:59:51 -0500

    save

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -10,13 +10,24 @@
 //add a process, either a new one or one that
 //had been running on the CPU and has been preempted
 void Scheduler::add(PCB p) {
+	Scheduler::ready_q->push(p);
 }
-
+//get next process
 PCB Scheduler::getNext() {
+	Scheduler::ready_q->pop();
 }
-
+//returns true if there are no  jobs in the readyQ
+//false otherwise
 bool Scheduler::isEmpty() {
+	return Scheduler::ready_q->empty();
 }
 
+//if process has completed (used all its remaining_cpu_time) or
+//if we are using a preemptive scheduling algorithm and the
+//timeslice is over then its time to switch processes
+//returns:
+//true - switch processes
+//false - do not switch
 bool Scheduler::time_to_switch_processes(int tick_count, PCB &p) {
+
 }
